87ccf2566a5a7c947b8512bdfb45d2a748cfefce
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-11-16 20:48:59 +0300

    comments and prettier in index file

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-const express=require("express");
-const app=express();
-// const env=require("dotenv");
-// env.config();
+const express = require("express");
+const app = express();
 app.use(express.json());
-// const cors = require("cors")
-// app.use(cors());
-const todos=["coding","eating","sleep"];
 
-app.get("/todos",(req,res)=>{
-    console.log("done");
+//array of todo item to applying CRUD operation
+const todos = ["coding", "eating", "sleep"];
+
+// get items from todos array
+app.get("/todos", (req, res) => {
+  console.log("done");
+  res.status(200).json(todos);
+});
+
+// create new item in todos array
+app.post("/createTodo", (req, res) => {
+  const { item } = req.body;
+  todos.push(item);
+  res.status(201).json(todos);
+});
+
+//update specific item in todos array by id
+app.put("/updateItem", (req, res) => {
+  const { item } = req.body;
+  const { id } = req.body;
+  try {
+    todos.splice(id, 1, item);
     res.status(200).json(todos);
-})
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-app.post("/createTodo",(req,res)=>{
-    const {item}=req.body;
-    todos.push(item);
-    res.status(201).json(todos);
-})
+//delete specific item in todos array
+app.delete("/deleteItem", (req, res) => {
+  //const {item}=req.body;
+  const { id } = req.body;
+  try {
+    todos.splice(id, 1);
+    res.status(200).json(todos);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-app.put("/updateItem",(req,res)=>{
-    const {item}=req.body;
-    const {id}=req.body;
-    try{
-        todos.splice(id,1,item);
-        res.status(200).json(todos)
-    }catch(err){
-        console.log(err);
-    }
-})
+const PORT = process.env.PORT || 4000;
 
-app.delete("/deleteItem",(req,res)=>{
-    //const {item}=req.body;
-    const {id}=req.body;
-    try{
-        todos.splice(id,1);
-        res.status(200).json(todos)
-    }catch(err){
-        console.log(err);
-    }
-})
-
-const PORT=process.env.PORT || 4000;
-
-app.listen(PORT,()=>{
-    console.log(`our server run on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`our server run on port ${PORT}`);
+});
